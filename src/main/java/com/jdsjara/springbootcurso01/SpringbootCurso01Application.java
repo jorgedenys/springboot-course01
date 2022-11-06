241788cb28c980bcc084ee0f10b34dd1bd66acf5
Jorge Denys <jorgedenys@gmail.com>
2022-11-06 18:31:43 -0300

    criando o projeto

---
+++ src/main/java/com/jdsjara/springbootcurso01/SpringbootCurso01Application.java
@@ -0,0 +1,13 @@
+package com.jdsjara.springbootcurso01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCurso01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCurso01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jdsjara/springbootcurso01/SpringbootCurso01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdsjara.springbootcurso01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCurso01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
